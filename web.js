563e6c9ed205b8bd56b774f9e6fdae5cf0cb4a19
Eran Marom <Eran@narnia.local>
2013-08-26 22:43:43 +0300

    added note about access to localhost from a remote device

--- web.js
+++ web.js
@@ -11,7 +11,8 @@ app.get('/', function(request, response) {
 var port = process.env.PORT || 8080; // may choose other ports as well, usually >1024
 // for viewing on local computer use one of the following:
 // http://localhost:8080
-// http://hostname:8080 (replace hostname with the output of 'hostname' command executed in the terminal. e.g. http://narnia.local:8080)
+// http://hostname:8080 (replace hostname with the output of 'hostname' command executed in the terminal). e.g. http://narnia.local:8080
+// from another device on the same network, find local ip (using '>> ifconfig | grep inet'), and then add the port. e.g. http://10.0.0.1:8080
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
