8c5c6e89dc01e6d2cfa04bd153c6a7cd36ddc548
Eran Marom <Eran@narnia.local>
2013-08-26 22:34:43 +0300

    clarifications on localhost

--- web.js
+++ web.js
@@ -8,7 +8,10 @@ app.get('/', function(request, response) {
   response.send(buffer.toString('utf-8'));
 });
 
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 8080; // may choose other ports as well, usually >1024
+// for viewing on local computer use one of the following:
+// http://localhost:8080
+// http://hostname:8080 (replace hostname with the output of 'hostname' command executed in the terminal. e.g. http://narnia.local:8080)
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
